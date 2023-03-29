2f5f4c531692749e53adb5f262a359688ae257c9
joao-marques-a22108693 <117601737+joao-marques-a22108693@users.noreply.github.com>
2023-03-29 09:33:04 +0100

    Commit

--- lab5/js/playground.js
+++ lab5/js/playground.js
@@ -1,5 +1,23 @@
 const calcButton = document.querySelector('#calcButton');
+const clearButton = document.querySelector('#clear');
+const result = document.querySelector('#resultado')
 
-calcButton.onclick = () => {
+document.querySelectorAll('#calc > button').forEach(button => {
+    if (button.innerHTML != '=' && button.innerHTML != 'C') {
+        button.onclick = function() {
+            result.value += button.innerHTML;
+        }
+    }
+});
+
+calcButton.onclick = function() {
+    result.value = eval(result.value);
+}
+
+clearButton.onclick = function() {
+    result.value = '';
+}
+
+/* calcButton.onclick = () => {
     document.getElementById('resultado').innerHTML = eval(document.getElementById('calc').value);
-};
+}; */
