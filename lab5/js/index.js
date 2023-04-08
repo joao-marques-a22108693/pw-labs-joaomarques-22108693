00df02756e61ee1b3c4b78b128a94601ed497e7b
joao-marques-a22108693 <117601737+joao-marques-a22108693@users.noreply.github.com>
2023-04-08 13:11:44 +0100

    Commit

--- lab5/js/index.js
+++ lab5/js/index.js
@@ -1,5 +1,15 @@
 const header = document.getElementById('header');
+const theme_toggle = document.getElementById('theme-toggle');
 
 window.onscroll = function() {
-    header.classList.add(['header-scrolled']);
+    if (scrollY == 0) {
+        header.classList.remove(['header-scrolled']);
+    } else if (!header.classList.contains('header-scrolled')) {
+        header.classList.add(['header-scrolled']);
+    }
+}
+
+function toggle_theme() {
+    const label = theme_toggle.children[0];
+    label.innerHTML = label.innerHTML == 'dark_mode' ? 'light_mode' : 'dark_mode';
 }
