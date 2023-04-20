c6f996568fbca8a6eb3255a8505ee08b580d8dc9
João <joao@DESKTOP.lan>
2023-04-20 18:06:32 +0100

    Commit

--- lab5/js/index.js
+++ lab5/js/index.js
@@ -1,5 +1,6 @@
 const header = document.getElementById('header');
 const theme_toggle = document.getElementById('theme-toggle');
+const menu_button = document.getElementById('menu-toggle');
 
 window.onscroll = function() {
     if (scrollY == 0) {
@@ -11,11 +12,13 @@ window.onscroll = function() {
 
 function toggle_theme() {
     const label = theme_toggle.children[0];
+    const menu_label = menu_button.children[0];
     const opposite_theme = label.innerHTML == 'dark_mode' ? 'dark' : 'light';
     const current_theme = opposite_theme == 'dark' ? 'light' : 'dark';
 
     label.innerHTML = current_theme + '_mode';
     label.style.color = current_theme == 'light' ? 'white' : 'black';
+    menu_label.style.color = current_theme == 'light' ? 'white' : 'black';
 
     for (const e of document.querySelectorAll(`.${current_theme}`)) {
         e.classList.replace(current_theme, opposite_theme);
