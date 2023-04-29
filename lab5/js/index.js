9d674a863436ecb06b749e8c74bc9a12eb496ef2
João <joao@unknown04421a2b1c7f.lan>
2023-04-29 23:32:14 +0100

    Commit

--- lab5/js/index.js
+++ lab5/js/index.js
@@ -1,14 +1,24 @@
 const menu = document.getElementById('menu');
 
+for (const e of menu.children) {
+    e.onclick = function () {
+        toggle_menu();
+    }
+}
+
 window.onscroll = function() {
     if (scrollY == 0) {
         header.classList.remove(['header-scrolled']);
     } else if (!header.classList.contains('header-scrolled')) {
         header.classList.add(['header-scrolled']);
     }
-}
+};
 
 
 function toggle_menu() {
-    menu.style.display = menu.style.display != 'flex' ? 'flex' : 'none';
+    if (!menu.classList.contains('open')) {
+        menu.classList.add('open');
+    } else {
+        menu.classList.remove('open');
+    }
 }
--- lab5/js/playground.js
+++ lab5/js/playground.js
@@ -2,6 +2,7 @@ const calcButton = document.querySelector('#calcButton');
 const clearButton = document.querySelector('#clear');
 const result = document.querySelector('#resultado');
 const img_txt = document.querySelector('#img-txt');
+const nome_txt = document.querySelector('#nome');
 
 document.querySelector('#date').innerHTML = Date();
 
@@ -11,7 +12,7 @@ window.onscroll = function() {
     } else if (!header.classList.contains('header-scrolled')) {
         header.classList.add(['header-scrolled']);
     }
-}
+};
 
 document.querySelectorAll('#calc > button').forEach(button => {
     if (button.innerHTML != '=' && button.innerHTML != 'C') {
@@ -23,14 +24,22 @@ document.querySelectorAll('#calc > button').forEach(button => {
 
 calcButton.onclick = function() {
     result.value = eval(result.value);
-}
+};
 
 clearButton.onclick = function() {
     result.value = '';
-}
+};
 
 img_txt.onkeypress = function(event) {
     if (event.which == 13) {
         document.querySelector('#img-lbl').innerHTML = img_txt.value;
+    }
+};
+
+nome_txt.onkeypress = function(event) {
+    if (event.which == 13) {
+        document.querySelector('#header > a').innerHTML = nome_txt.value;
+        document.querySelector('#date').innerHTML = nome_txt.value;
+        document.querySelector('#img-lbl').innerHTML = nome_txt.value;
     }
 }
